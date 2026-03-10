7f8243827a350080a0d050e034b7c2b0e7230e20
MVP-03 <pranaysurya4@gmail.com>
2026-03-10 10:30:00 +0530

    refactor: extract pacing thresholds and stage config into constants

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+PACING_THRESHOLDS = {
+    'ahead':    0.80,
+    'behind':   1.20,
+}
+
+DEFAULT_STAGE_DAYS = 14
+
+WIN_STAGE    = 'closed-won'
+LOSS_STAGE   = 'closed-lost'
+ACTIVE_STAGE = 'closed'
+
+CSV_DATE_FORMAT = '%Y-%m-%d'
